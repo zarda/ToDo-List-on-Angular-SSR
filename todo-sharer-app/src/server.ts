d24af545959f3df21d36c7333b123abf42649085
Jan Hengtai <zarda0@hotmail.com>
2025-10-18 23:43:18 +0800

    Fix typo

--- todo-sharer-app/src/server.ts
+++ todo-sharer-app/src/server.ts
@@ -1,43 +1,49 @@
-import {
-  AngularNodeAppEngine,
-  createNodeRequestHandler,
-  isMainModule,
-  writeResponseToNodeResponse,
-} from '@angular/ssr/node';
-import express from 'express';
+import { AngularNodeAppEngine, createNodeRequestHandler, isMainModule, writeResponseToNodeResponse } from '@angular/ssr/node'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import express from 'express'
 
-const app = express();
-// The Angular app engine, which handles rendering and serving static files.
-// It is created without arguments and discovers the necessary files automatically.
-const angularApp = new AngularNodeAppEngine();
+const serverDistFolder = dirname(fileURLToPath(import.meta.url))
+const browserDistFolder = resolve(serverDistFolder, '../browser')
+
+const app = express()
+const angularApp = new AngularNodeAppEngine()
+
+/**
+ * Serve static files from /browser
+ */
+app.use(
+	express.static(browserDistFolder, {
+		maxAge: '1y',
+		index: false,
+		redirect: false
+	})
+)
 
 /**
  * Handle all other requests by rendering the Angular application.
  */
 app.use((req, res, next) => {
-  angularApp
-    .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
-    .catch(next);
-});
+	angularApp
+		.handle(req)
+		.then((response) =>
+			response ? writeResponseToNodeResponse(response, res) : next(),
+		)
+		.catch(next)
+})
 
 /**
- * Start the server if this module is the main entry point, or it is ran via PM2.
+ * Start the server if this module is the main entry point.
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
-if (isMainModule(import.meta.url) || process.env['pm_id']) {
-  // The port the server will listen on, defined by the `PORT` environment variable.
-  const port = Number(process.env['PORT']) || 8080;
-  // The host the server will listen on.
-  const host = '0.0.0.0';
-  app.listen(port, host, () => {
-    console.log(`Node Express server listening on http://${host}:${port}`);
-  });
+if (isMainModule(import.meta.url)) {
+	const port = process.env['PORT'] || 8080
+	app.listen(port, () => {
+		console.log(`Node Express server listening on http://localhost:${port}`)
+	})
 }
 
 /**
  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
  */
-export const reqHandler = createNodeRequestHandler(app);
+export default createNodeRequestHandler(app)
