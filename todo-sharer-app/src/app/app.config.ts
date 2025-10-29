b3d858a7e2f59ca63721f164beb18e8753245527
Jan Hengtai <zarda0@hotmail.com>
2025-10-29 23:20:18 +0800

    Implement feature to switch theme: light/dark

--- todo-sharer-app/src/app/app.config.ts
+++ todo-sharer-app/src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { environment } from './environment/environment';
@@ -10,6 +11,7 @@ import { environment } from './environment/environment';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideHttpClient(withFetch()),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
--- todo-sharer-app/src/app/app.ts
+++ todo-sharer-app/src/app/app.ts
@@ -1,16 +1,51 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, OnInit, Renderer2, Inject, PLATFORM_ID } from '@angular/core';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Navigation } from './navigation/navigation';
 import { AuthService } from './auth/auth.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navigation], // RouterOutlet is still needed here
+  imports: [RouterOutlet, Navigation, MatIconModule], // RouterOutlet is still needed here
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('todo-sharer-app');
   protected readonly authService = inject(AuthService);
+  private readonly document = inject(DOCUMENT);
+  private readonly renderer = inject(Renderer2);
+  isDarkMode = signal(false);
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const storedTheme = localStorage.getItem('theme');
+      if (storedTheme === 'dark') {
+        this.isDarkMode.set(true);
+        this.renderer.addClass(this.document.documentElement, 'dark-theme');
+      } else {
+        this.isDarkMode.set(false);
+        this.renderer.removeClass(this.document.documentElement, 'dark-theme');
+      }
+    }
+  }
+
+  toggleTheme() {
+    this.isDarkMode.update(value => !value);
+    if (this.isDarkMode()) {
+      this.renderer.addClass(this.document.documentElement, 'dark-theme');
+      if (isPlatformBrowser(this.platformId)) {
+        localStorage.setItem('theme', 'dark');
+      }
+    } else {
+      this.renderer.removeClass(this.document.documentElement, 'dark-theme');
+      if (isPlatformBrowser(this.platformId)) {
+        localStorage.setItem('theme', 'light');
+      }
+    }
+  }
 }
