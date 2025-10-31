6349716763c267473ea3c958eff2a895e8ea0fca
Jan Hengtai <zarda0@hotmail.com>
2025-10-31 21:25:26 +0800

    Use hybrid rendering to cowork SSR & CSR
     - SSR for login page
     - CSR for main page, which needs more interactions

--- todo-sharer-app/src/app/app.routes.server.ts
+++ todo-sharer-app/src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
-    renderMode: RenderMode.Server
+    path: '',
+    renderMode: RenderMode.Client  // CSR for todo-list page
+  },
+  {
+    path: 'login',
+    renderMode: RenderMode.Server  // SSR for login page
   }
 ];
