832997c91a933145f4bceb1b2b622e20144d51bb
Jan Hengtai <zarda0@hotmail.com>
2025-11-02 01:23:22 +0800

    Include environment.ts

---
+++ todo-sharer-app/src/app/environment/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyBFKBoJMGo9AJcy7trjUGPzXRfyFurKucw",
+        authDomain: "todo-sharder-angular-ssr.firebaseapp.com",
+        projectId: "todo-sharder-angular-ssr",
+        storageBucket: "todo-sharder-angular-ssr.firebasestorage.app",
+        messagingSenderId: "394518549753",
+        appId: "1:394518549753:web:6e79c0c2b83a4fb047be28",
+        measurementId: "G-549WHRW7ZB"
+    }
+};
